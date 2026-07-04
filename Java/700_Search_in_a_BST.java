ec31c042b855ce665623d297a5f9b2227463b5e0
Atifuddin <matifudd@gitam.in>
2026-07-04 15:52:52 +0530

    Create 700_Search_in_a_BST.java

---
+++ Java/700_Search_in_a_BST.java
@@ -0,0 +1,18 @@
+# LeetCode Solutions
+
+This repository contains my Java solutions to LeetCode problems.
+
+## Language
+- Java
+
+## Problems Solved
+
+| Problem No. | Problem Name | Difficulty |
+|-------------|--------------|------------|
+| 700 | Search in a Binary Search Tree | Easy |
+| 701 | Insert into a Binary Search Tree | Medium |
+| 98 | Validate Binary Search Tree | Medium |
+| 102 | Binary Tree Level Order Traversal | Medium |
+| 450 | Delete Node in a BST | Medium |
+
+I will keep updating this repository as I solve more problems.
